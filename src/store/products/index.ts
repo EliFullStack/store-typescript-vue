626751655894359c7cffa262ef2cf5faf60fe00c
unknown <elisabet.v.alvarez@gmail.com>
2023-01-29 23:16:23 +0100

    getters added

--- src/store/products/index.ts
+++ src/store/products/index.ts
@@ -1,11 +1,13 @@
 import { Module } from "vuex";
 import { IState } from "@/store";
 import state, { IProductsState } from "./state";
+import getters from "./getters";
 
 
 const productsModule: Module<IProductsState, IState> = {
     namespaced: true,
     state,
+    getters
     
 }
 
