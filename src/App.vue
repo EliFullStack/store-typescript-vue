deeb70e8a20674160149571c82b4a005a18f6f81
unknown <elisabet.v.alvarez@gmail.com>
2023-01-29 11:42:06 +0100

    NavBar component created and added to App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/profile">Profile</router-link>
-  </nav>
+  </nav> -->
+  <NavBar class="flex justify-between"
+  title="Wellcome to MyStore" 
+    :links="[
+      { label: 'Facebook', link: 'http://facebook.com' },
+      { label: 'Instagram', link: 'http://instagram.com' }
+    ]"
+    @buttonClicked="alertClick($event)"
+  />
   <router-view/>
 </template>
+
+<script lang="ts">
+import NavBar from "./components/NavBar.vue"
+import {Link} from "@/interfaces/link"
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AppComponent',
+  components: {
+    NavBar
+  },
+  setup() {
+    return {
+      alertClick: (link: Link) => alert(link.label + ' Pulsado!'),
+    }
+  }
+  
+})
+</script>
+
 
 <style>
 #app {
@@ -16,15 +44,7 @@
 }
 
 nav {
-  padding: 30px;
+  padding: 5px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,81 @@
+<template>
+    <nav>
+        <div class="title">
+            <h4 class="text-xl ml-5">{{title}}</h4>
+        </div>       
+        <div class="text-base mx-10 flex flex-col" v-if="links">
+            <button 
+                v-for="link in links"
+                :key="link.label"
+                @click="onClick(link)"
+                >{{ link.label }}  
+            </button>
+        </div>        
+    </nav>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Link } from '@/interfaces/link';
+import { useStore } from 'vuex';
+
+
+interface NavBarProps {
+    title: string,
+    links: Link [],
+    color?: string
+}
+
+export default defineComponent({
+    name: 'NavBar',
+    emits: ['buttonClicked'],
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        links: {
+            type: Array as PropType <Link []>,
+            default () : Link [] {
+                return [
+                {label: 'Instagram', link: 'https://www.instagram.com/'},
+                {label: 'Facebook', link: 'https://www.facebook.com/'}
+                ]
+            }
+        },
+        color: {
+            type: String
+        }
+    },
+    setup(props: NavBarProps, ctx) {
+        const store = useStore()
+        return {
+            
+            onClick: (link: Link) => {
+                console.log('onClick', link)
+                ctx.emit('buttonClicked', link)
+            },
+        }
+    }
+})
+</script>
+
+
+<style scoped>
+nav {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgb(35,47,62);
+  color: white;
+}
+/*rgb(19,25,33)*/
+h4 {
+    color: white;
+}
+
+</style>
+
+
