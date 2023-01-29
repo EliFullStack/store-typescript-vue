c2cc7dd39b1b39a8fefa03e83dfaf239bc5477ec
unknown <elisabet.v.alvarez@gmail.com>
2023-01-29 23:19:57 +0100

    mutations added

--- src/store/products/index.ts
+++ src/store/products/index.ts
@@ -2,12 +2,14 @@ import { Module } from "vuex";
 import { IState } from "@/store";
 import state, { IProductsState } from "./state";
 import getters from "./getters";
+import mutations from "./mutations";
 
 
 const productsModule: Module<IProductsState, IState> = {
     namespaced: true,
     state,
-    getters
+    getters,
+    mutations
     
 }
 
