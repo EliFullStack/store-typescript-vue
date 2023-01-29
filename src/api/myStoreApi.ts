a885482ba173a86972eb48ed12a6020123a16d27
unknown <elisabet.v.alvarez@gmail.com>
2023-01-29 17:32:20 +0100

    axios installed and axios instance created

---
+++ src/api/myStoreApi.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosHeaders } from "axios";
+
+const myStoreApi = axios.create({
+    baseURL: 'https://api.escuelajs.co/api/v1'
+})
+
+myStoreApi.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token') ?? '';
+    if(token) {
+      (config.headers as AxiosHeaders).set('Authorization', `Bearer ${token}`);//JWT
+    } else {
+      
+    }   
+    return config
+})
+
+export default myStoreApi;
